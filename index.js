4046fff12eb340c5c35c249d4db8e11d3833c162
Andy <willisustc@gmail.com>
2020-06-05 12:22:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
     return res.send("hello world");
+})
+*/
+app.get('/', function (req, res) {
+  child_process.exec(
+    'gzip ' + req.query.file_path,
+    function (err, data) {
+      console.log('err: ', err)
+      console.log('data: ', data);
+    });
+  res.send('Hello World!')
 })
 
 app.get('/hello', function (req, res) {
